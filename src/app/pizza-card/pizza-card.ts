4128d149620e2d8816c05f0f7fc947a2c91dabd5
Ivan <ivancarrillovela@gmail.com>
2025-11-13 11:42:45 +0100

    refactor: Simplify pizza card component code and remove unnecessary comments

--- src/app/pizza-card/pizza-card.ts
+++ src/app/pizza-card/pizza-card.ts
@@ -6,49 +6,33 @@ import { PizzaModel } from '../models/PizzaModel';
 @Component({
   selector: 'app-pizza-card',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule], // Importamos ReactiveFormsModule
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './pizza-card.html',
+  styleUrl: './pizza-card.scss'
 })
 export class PizzaCard {
-
-  // 1. Recibimos la pizza (Input)
+  
   pizza = input.required<PizzaModel>();
-
-  // 2. Emitimos el evento de agregar (Output)
   agregar = output<{ pizza: PizzaModel, cantidad: number }>();
 
-  // 3. Creamos el FormGroup para el formulario (Reactive Forms)
   formularioCantidad = new FormGroup({
     cantidad: new FormControl(1, [Validators.required, Validators.min(1)])
   });
 
-  // 4. Getter para el template (como en receta-form.ts)
   get cantidad() {
     return this.formularioCantidad.get('cantidad');
   }
 
-  // 5. Método para enviar el formulario
   enviarFormulario() {
-    if (this.formularioCantidad.invalid) {
-      return;
-    }
-
+    if (this.formularioCantidad.invalid) return;
     const cantidadSeleccionada = this.cantidad?.value ?? 1;
-
-    // Emitimos el evento al componente padre
     this.agregar.emit({
       pizza: this.pizza(),
       cantidad: cantidadSeleccionada
     });
-
-    // Reseteamos el valor a 1
     this.formularioCantidad.reset({ cantidad: 1 });
   }
 
-  /**
-   * Método que devuelve la ruta de la imagen del ingrediente
-   * basado en el nombre del ingrediente.
-   */
   public getIconoIngrediente(ingrediente: string): string {
     const nombre = ingrediente.toLowerCase();
 
